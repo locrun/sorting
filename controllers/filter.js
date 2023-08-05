2a5475789bba0c53b1b495552c907ef099564820
Runar <github.runar@gmail.com>
2023-08-06 02:08:43 +0300

    update filterAndSortData

--- controllers/filter.js
+++ controllers/filter.js
@@ -3,24 +3,24 @@ import { include } from "./include.js"
 import { keyFilter } from "./keyFilter.js"
 import { sortData as sort } from "./sortData.js"
 
-const filterData = (data, rule) => {
-  if (!Array.isArray(data) || !Array.isArray(rule)) {
-    if (!Array.isArray(data)) {
-      throw new Error("Data is not an array")
-    }
-    if (!Array.isArray(rule)) {
-      throw new Error("Rule is not an array")
-    }
+export function filterAndSortData(data, rule) {
+  if (!Array.isArray(data)) {
+    throw new Error("Data is not an array")
   }
+  if (!Array.isArray(rule.include)) {
+    throw new Error("Include rule  is not an array")
+  }
+  if (!Array.isArray(rule.exclude)) {
+    throw new Error("Exclude rule is not an array")
+  }
+
   let result = [...data]
+
   result = include(result, rule)
   result = exclude(result, rule)
-  return result
-}
 
-export function filterAndSortData(data, rule) {
-  const filteredData = filterData(data, rule)
-  const keyFilterData = keyFilter(filteredData, rule.sort_by)
+  const keyFilterData = keyFilter(result, rule.sort_by)
   const sortData = sort(keyFilterData)
+
   return { result: sortData }
 }
